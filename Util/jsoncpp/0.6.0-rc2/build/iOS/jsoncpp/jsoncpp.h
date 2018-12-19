c2919a1bb1a7a9568a2c43e2c9d779495d3f4a84
kael ai <kael.ai@dingtone.me>
2018-12-19 16:16:08 +0800

    commit jsoncpp file

---
+++ Util/jsoncpp/0.6.0-rc2/build/iOS/jsoncpp/jsoncpp.h
@@ -0,0 +1,13 @@
+//
+//  jsoncpp.h
+//  jsoncpp
+//
+//  Created by Taylor Wei on 2/26/13.
+//  Copyright (c) 2013 Taylor Wei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface jsoncpp : NSObject
+
+@end
---
+++ Util/jsoncpp/0.6.0-rc2/build/iOS/jsoncpp/jsoncpp.m
@@ -0,0 +1,13 @@
+//
+//  jsoncpp.m
+//  jsoncpp
+//
+//  Created by Taylor Wei on 2/26/13.
+//  Copyright (c) 2013 Taylor Wei. All rights reserved.
+//
+
+#import "jsoncpp.h"
+
+@implementation jsoncpp
+
+@end
