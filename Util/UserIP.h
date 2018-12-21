99e300c6d7a22038faf1a91c0c79ef882232434b
kael ai <kael.ai@dingtone.me>
2018-12-21 17:23:51 +0800

    manage user ip

---
+++ Util/UserIP.h
@@ -0,0 +1,17 @@
+//
+//  UserIP.h
+//  PFIMCLient
+//
+//  Created on 10/1/12.
+//  Copyright 2011 Dingtone,Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#include "Jutype.h"
+@interface UserIP : NSObject
+
+@property UserID_t userId;
+@property UInt32 site;
+@property UInt32 host;
+
+@end
---
+++ Util/UserIP.m
@@ -0,0 +1,26 @@
+//
+//  UserIP.m
+//  PFIMCLient
+//
+//  Created on 10/1/12.
+//  Copyright 2012 Dingtone,Inc. All rights reserved.
+//
+
+#import "UserIP.h"
+
+@implementation UserIP
+@synthesize userId;
+@synthesize host;
+@synthesize site;
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
